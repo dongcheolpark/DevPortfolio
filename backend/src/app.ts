01e2499120b5658c107652bee45995fe96a42301
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-06-19 21:32:42 +0900

    vue - express 연동

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,18 @@ import { aboutRouter } from './router/aboutrouter';
 import { testApiRouter } from './router/testApi';
 import { home } from './router/home';
 const app = express();
- 
+
+/// <backend-router> 
 app.use('/aboutrouter',aboutRouter);
 app.use('/testapi',testApiRouter);
+/// </backend-router> 
+
+/// <frontend-router> 
 app.use('/',home);
+app.use('/about',home);
+/// </frontend-router> 
+
+app.use(express.static('public'));
 
 app.listen(3000, () => {
   console.log('listen t0 3000')
