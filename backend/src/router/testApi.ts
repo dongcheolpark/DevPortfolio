1d78a54f61563a7adaa75a05599355047b793dbc
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-07-04 16:26:17 +0900

    에러 처리 테스트

--- backend/src/router/testApi.ts
+++ backend/src/router/testApi.ts
@@ -2,20 +2,22 @@ import express, { Request, Response, NextFunction } from 'express';
 import connection from "../lib/DbConfig";
 
 export const testApiRouter = express.Router();
-testApiRouter.use(function(req, res, next) {
-		next();
-	});
+testApiRouter.use(function (req, res, next) {
+	next();
+});
 testApiRouter.get('/getTest', async (req, res) => {
-	try {
-		connection.query('SELECT * from test', (error, rows) => {
+	connection.query('SELECT * from test', (error, rows) => {
+		try {
 			if (error) throw error;
 			console.log('User info is: ', rows);
 			res.send(rows);
-		});
-	} catch (err) {
-		console.log(err);
-		res.send({
-			error: 'Can"t read api data',
-		});
-	}
-  }); 
+		}
+		catch (err) {
+			console.log(err);
+			res.send({
+				error: 'Can"t read api data',
+			});
+
+		}
+	});
+}); 
